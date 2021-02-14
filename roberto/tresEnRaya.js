3085336f8d3e4d5e46b62b9f9ae27e1d87d264ef
rovilram <robertovillaresrama@gmail.com>
2021-02-14 10:30:20 +0100

    functional beta

--- roberto/tresEnRaya.js
+++ roberto/tresEnRaya.js
@@ -32,38 +32,59 @@ function isOcuppied(coord, playerArray) {
     return false;
 }
 
-function playerTurn(playerArrayA, playerArrayB) {
+function board(arrayA, playerA, arrayB, playerB) {
+    let boardArray = [[undefined, undefined, undefined], [undefined, undefined, undefined], [undefined, undefined, undefined]];
+    for (let i = 0; i < arrayA.length; i++) {
+        boardArray[arrayA[i][0] - 1][arrayA[i][1] - 1] = playerA;
+    }
+    for (let i = 0; i < arrayB.length; i++) {
+        boardArray[arrayB[i][0] - 1][arrayB[i][1] - 1] = playerB;
+    }
+    return boardArray;
+}
+function playerTurn(playerArrayA, playerArrayB, player) {
     let firstLoop = true;
     let coord = [];
-
+    let mainArray;
+    if (player === "A") {
+        mainArray = playerArrayA;
+    }
+    else {
+        mainArray = playerArrayB;
+    }
 
     while (firstLoop || ((isOcuppied(coord, playerArrayA) || isOcuppied(coord, playerArrayB)))) {
-        let turnV = parseInt(prompt("Dime las coordenada vertical de tu jugada"));
-        let turnH = parseInt(prompt("Dime las coordenada horizontal de tu jugada"));
+        let turnV = parseInt(prompt(`Jugador ${player}: dime las coordenada vertical de tu jugada`));
+        let turnH = parseInt(prompt(`Jugador ${player}: dime las coordenada horizontal de tu jugada`));
         coord = [turnV, turnH];
         firstLoop = false;
     }
-    console.log("coordenadas escogidas", coord);
-    playerArrayA[playerArrayA.length] = coord;
-    console.log("array jugador:", JSON.stringify(playerArrayA));
+    //console.log("coordenadas escogidas", coord);
+    mainArray[mainArray.length] = coord;
+    //console.log("array jugador " + player + ":", JSON.stringify(mainArray));
+    boardArray = board(playerArrayA, "A", playerArrayB, "B");
+    for (let i = 0; i < boardArray.length; i++) {
+        console.log(JSON.stringify(boardArray[i]));
+    }
 
-    if (winner(playerArrayA)) {
-        console.log("has ganado");
+    if (winner(mainArray)) {
+    //    console.log(player, "has ganado");
         return true;
     } else {
-        console.log("NO has ganado");
+    //    console.log(player, "NO has ganado");
         return false;
     }
 }
+
 
 function tresEnRaya() {
     let playerArrayA = [];
     let playerArrayB = [];
     while ((playerArrayA.length + playerArrayB.length) < 9) {
-        console.log("JUGADOR 1");
-        if (playerTurn(playerArrayA, playerArrayB)) return "A";
-        console.log("JUGADOR 2");
-        if (playerTurn(playerArrayB, playerArrayA)) return "B";
+ //       console.log("JUGADOR A");
+        if (playerTurn(playerArrayA, playerArrayB, "A")) return "A";
+ //       console.log("JUGADOR B");
+        if (playerTurn(playerArrayA, playerArrayB, "B")) return "B";
     }
 }
 
